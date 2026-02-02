425165d75616264d2483840c752b9d66778c16a8
ritikraj0257-lab <ritikraj0257@gmail.com>
2026-02-02 11:08:01 +0530

    Create multiplication table.c

---
+++ multiplication table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; ++i) {
+        printf("\nMultiplication table of %d:\n", i);
+
+        for (j = 1; j <= 10; ++j) {
+            printf("%d x %d = %d\n", i, j, i * j);
+        }
+    }
+
+    return 0;
+}
